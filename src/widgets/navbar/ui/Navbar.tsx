031e30914ab8201ee9b26bff456a5e4283787500
changheonkim <heon5652@gmail.com>
2025-01-14 21:44:41 +0900

    add: 디자인 수정 및 애니메이션 추가

--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -1,18 +1,14 @@
 export default function Navbar() {
   return (
-    <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
-      <div className="container mx-auto flex items-center justify-between py-4 px-6">
-        <div className="text-xl font-bold">Naro's Portfolio</div>
+    <header className="fixed top-0 left-0 w-full bg-black shadow-md z-50">
+      <div className="container mx-auto flex items-center justify-between w-[1000px] text-white py-4 px-6">
+        <div className="text-xl font-bold">Naro's portfolio</div>
         <nav className="flex space-x-4">
-          <a href="#home" className="text-gray-700 hover:text-blue-500">
-            Home
-          </a>
-          <a href="#about" className="text-gray-700 hover:text-blue-500">
-            About
-          </a>
-          <a href="#contact" className="text-gray-700 hover:text-blue-500">
-            Contact
-          </a>
+          <a href="#home">About me</a>
+          <a href="#about">Skill</a>
+          <a href="#contact">Archiving</a>
+          <a href="#contact">Project</a>
+          <a href="#contact">Career</a>
         </nav>
       </div>
     </header>
