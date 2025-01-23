c6b52ca4bb042eb61255386f88dd0f6d83f54ed2
naro.kim <naro.kim@kakaocorp.com>
2025-01-23 17:34:00 +0900

    fix: aboutMe 섹션 구선 변경

--- src/features/aboutMeSection/AboutMeSection.tsx
+++ src/features/aboutMeSection/AboutMeSection.tsx
@@ -5,11 +5,31 @@ export default function AboutMeSection() {
   return (
     <SectionContainer>
       <Title>ABOUT ME</Title>
-      <div className="bg-white w-full h-[700px] text-black rounded flex flex-col justify-center items-center">
-        <div className="w-[50px] font-[14px] m-20 leading-8">
-          블로그 깃허브 이메일 이력서
+      <div className="w-full h-[700px] p-20 bg-white text-black rounded flex flex-col justify-center items-center">
+        <div className="w-[800px] h-[300px] font-[14px] flex justify-between items-center">
+          <div className="w-[250px] h-[250px] overflow-hidden rounded-full">
+            <img
+              src="https://m.wmelonshop.com/web/product/big/20200330/0c283ec52c8dca480a81a7f73df2fc33.jpg"
+              alt="Circular Image"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <div className="w-[500px] h-[50px] bg-gray-200 pl-8 rounded-xl flex items-center">
+              heon5652@gmail.com
+            </div>
+            <div className="w-[500px] h-[50px] bg-gray-200 pl-8 rounded-xl flex items-center">
+              GITHUB
+            </div>
+            <div className="w-[500px] h-[50px] bg-gray-200 pl-8 rounded-xl flex items-center">
+              BLOG
+            </div>
+            <div className="w-[500px] h-[50px] bg-gray-200 pl-8 rounded-xl flex items-center">
+              RESUME
+            </div>
+          </div>
         </div>
-        <div className="font-[14px] m-20 leading-8">
+        <div className="w-[800px] font-[14px] mt-4 leading-8">
           안녕하세요, 창의적이고 열정적인 주니어 프론트엔드 개발자 김창헌입니다.
           <br />
           최신 웹 기술을 바탕으로 UI/UX 디자인과 코드 최적화에 지속적으로
