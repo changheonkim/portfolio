5f2f64c7209e6e6dce14a45bf5d431f3d9f34f90
naro.kim <naro.kim@kakaocorp.com>
2025-02-01 15:35:26 +0900

    fix: skillSection 사이즈 조정

--- src/features/skillSection/ui/SkillSection.tsx
+++ src/features/skillSection/ui/SkillSection.tsx
@@ -1,20 +1,9 @@
-import { Comfortaa } from 'next/font/google';
-
-import SectionContainer from '@/widgets/sectionContainer';
 import Monitor from './Monitor';
-
-const comfortaa = Comfortaa({
-  variable: '--comfortaa', // CSS 변수 이름 수정
-  subsets: ['latin'], // 필요한 문자 집합 (한글 지원 필요 시 'latin-ext', 'korean' 추가)
-  weight: ['700'], // 사용할 폰트 가중치
-});
 
 export default function SkillSection() {
   return (
-    <SectionContainer>
-      <div
-        className={`w-full flex flex-col justify-center items-center gap-8 text-[#efefef] ${comfortaa.className}`}
-      >
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="w-[1300px] flex flex-col justify-center items-center gap-8 text-[#efefef]">
         <Monitor kind="FrontEnd Skills">
           <div>
             VueJs ReactJs TypeScript JavaScript ThreeJs HTML CSS TailwindCSS
@@ -32,7 +21,7 @@ export default function SkillSection() {
           </div>
         </Monitor>
 
-        <div className="flex justify-between gap-8">
+        <div className="w-full flex justify-between gap-[20px]">
           <div className="w-[620px] h-[400px] border-2 rounded-xl border-[#A8A9AD] flex p-4">
             vue
           </div>
@@ -41,6 +30,6 @@ export default function SkillSection() {
           </div>
         </div>
       </div>
-    </SectionContainer>
+    </div>
   );
 }
