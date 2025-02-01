baefa6d4cc6b1d09bf7c3f3796800036d1de75d6
naro.kim <naro.kim@kakaocorp.com>
2025-02-01 16:24:26 +0900

    update: aboutSection 스타일 변경

--- src/features/aboutMeSection/ui/AboutMeSection.tsx
+++ src/features/aboutMeSection/ui/AboutMeSection.tsx
@@ -7,42 +7,37 @@ import ContactText from './ContactText';
 import IntroduceText from './IntroduceText';
 
 export default function AboutMeSection() {
-  const { ref, inView } = useInView({ triggerOnce: false, threshold: 0.4 });
+  const { ref, inView } = useInView({ triggerOnce: false, threshold: 0.3 });
 
   return (
     <motion.div
       ref={ref}
-      className="w-full h-screen flex justify-center items-center"
+      className="w-full h-screen flex justify-center items-center relative"
       initial={{ x: '50vw', opacity: 0 }}
       animate={inView ? { x: 0, opacity: 1 } : { x: '50vw', opacity: 0 }}
       transition={{ type: 'spring', stiffness: 70, damping: 15 }}
     >
-      <div className="w-[80vw] h-[80vh] p-10 bg-[#e8e0d9] text-black rounded flex flex-col items-center">
-        <div className="w-[74vw] h-[70vh] font-[2em] flex justify-between relative">
-          {/* 좌측 섹션 */}
-          <div className="w-[34vw] flex flex-col justify-between">
-            <ContactText />
+      <div className="w-[70vw] h-[82vh] p-10 bg-[#dbdbdb] text-black rounded relative">
+        {/* 중앙 텍스트 섹션 */}
+        <div className="absolute inset-0 flex flex-col gap-[2em] justify-center items-center">
+          <ContactText />
+          <IntroduceText />
+        </div>
+
+        {/* 우측 하단 이미지 */}
+        <div className="absolute bottom-10 right-10">
+          <div className="w-[8vw] h-[8vw] rounded-full border border-gray-500">
+            <img
+              src="https://m.wmelonshop.com/web/product/big/20200330/0c283ec52c8dca480a81a7f73df2fc33.jpg"
+              alt="Circular Image"
+              className="w-full h-full rounded-full object-cover"
+            />
           </div>
+        </div>
 
-          {/* 가운데 세로 선 */}
-          <div className="absolute left-1/2 top-0 bottom-0 w-[1px] h-full bg-gray-600"></div>
-
-          {/* 우측 섹션 */}
-          <div className="w-[34vw] h-[60vh] relative flex flex-col items-center">
-            {/* 이미지 섹션 */}
-            <div className="w-[10vw] h-[13vw] mr-4 mt-4 absolute top-0 right-0 border border-gray-500">
-              <img
-                src="https://m.wmelonshop.com/web/product/big/20200330/0c283ec52c8dca480a81a7f73df2fc33.jpg"
-                alt="Circular Image"
-                className="w-full h-full object-cover"
-              />
-            </div>
-
-            {/* 텍스트 섹션 */}
-            <div className="flex flex-col items-center justify-center text-left mt-[32vh]">
-              <IntroduceText />
-            </div>
-          </div>
+        <div className="absolute bottom-10 left-10 flex flex-col gap-1">
+          <span>성균관대학교 미술 & 소프트웨어 전공 학점 3.6/4.5</span>
+          <span>1996.08.22</span>
         </div>
       </div>
     </motion.div>
--- src/features/aboutMeSection/ui/ContactText.tsx
+++ src/features/aboutMeSection/ui/ContactText.tsx
@@ -1,20 +1,10 @@
-import PostLine from './PostLine';
-
 export default function ContactText() {
   return (
-    <>
-      <div className="w-full flex flex-col gap-[10px]">
-        <PostLine>
-          <div className="w-[20vw] h-[5vh] flex items-center">GITHUB</div>
-        </PostLine>
-        <PostLine>
-          <div className="w-[20vw] h-[5vh] flex items-center">BLOG</div>
-        </PostLine>
-        <PostLine>
-          <div className="w-[20vw] h-[5vh] flex items-center">RESUME</div>
-        </PostLine>
-      </div>
-      <div>heon5652@gmail.com</div>{' '}
-    </>
+    <div className="flex gap-[10px] text-[1.8em] font-bold">
+      <span>GITHUB</span>
+      <span>BLOG</span>
+      <span>RESUME</span>
+      <span>EMAIL</span>
+    </div>
   );
 }
