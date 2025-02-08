40d4798dc31e7ca4b07cfb9eeadfcd2cbc860d99
naro.kim <naro.kim@kakaocorp.com>
2025-02-08 15:39:09 +0900

    update: ProjectSection 디자인 수정

--- src/features/projectSection/ProjectSection.tsx
+++ src/features/projectSection/ProjectSection.tsx
@@ -3,9 +3,14 @@ import SectionContainer from '@/widgets/sectionContainer';
 export default function ProjectSection() {
   return (
     <SectionContainer>
-      <div className="w-full bg-white p-[40px] flex flex-col gap-[40px]">
-        <div className="w-[40vw] h-[400px] border border-black rounded-xl"></div>
-        <div className="w-[40vw] h-[400px] border border-black rounded-xl"></div>
+      <div className="w-full bg-gray-200 p-[40px] pt-[80px] pb-[80px] flex flex-col items-center gap-[5vh]">
+        <div className="w-[60vw] h-[60vh] bg-black border border-black flex items-center justify-center rounded-xl">
+          <div className="w-[56vw] h-[52vh] bg-white rounded-xl"></div>
+        </div>
+
+        <div className="w-[60vw] h-[60vh] bg-black border border-black flex items-center justify-center rounded-xl">
+          <div className="w-[56vw] h-[52vh] bg-white rounded-xl"></div>
+        </div>
       </div>
     </SectionContainer>
   );
--- src/widgets/sectionContainer/SectionContainer.tsx
+++ src/widgets/sectionContainer/SectionContainer.tsx
@@ -2,7 +2,7 @@ import { SectionContainerProps } from './SectionContainer.type';
 
 export default function SectionContainer({ children }: SectionContainerProps) {
   return (
-    <div className="w-full flex justify-center items-center overflow-hidden">
+    <div className="w-full flex justify-center items-center overflow-hidden relative">
       {children}
     </div>
   );
