41fb61325b5dc41912fc456cf556aba8fa756c15
naro.kim <naro.kim@kakaocorp.com>
2025-02-15 15:10:18 +0900

    update: career Section 영역 수정

--- src/features/careerSection/CareerSection.tsx
+++ src/features/careerSection/CareerSection.tsx
@@ -4,8 +4,17 @@ export default function CareerSection() {
   return (
     <SectionContainer>
       <div className="w-full bg-gray-200 relative flex flex-col justify-center items-center">
-        <div className="w-[80vw] mt-[20vh] mb-[60px] ml-[40px] mr-[40px] rounded-xl bg-black">
-          <div className="h-[80vw]">dsdfs</div>
+        <div className="w-[80vw] mt-[20vh] mb-[20vh] ml-[5vw] mr-[5vw] rounded-xl bg-black">
+          <div className="w-full p-10 text-white">
+            <div className="flex">
+              <div className="w-[20vw] bg-gray-200 p-10">
+                <div className="w-[10vw] h-[10vw] bg-red-100"></div>
+              </div>
+              <div className="w-[80vw] bg-gray-400 p-10">
+                sodudsafdnaslfbslbdlshbhl
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </SectionContainer>
