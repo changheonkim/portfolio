b5f4bb212be1f03e6d382818eb8d63cb59574be8
naro.kim <naro.kim@kakaocorp.com>
2025-02-17 20:16:16 +0900

    update: careerSection 디자인 수정

--- src/features/careerSection/CareerSection.tsx
+++ src/features/careerSection/CareerSection.tsx
@@ -7,11 +7,26 @@ export default function CareerSection() {
         <div className="w-[80vw] mt-[20vh] mb-[20vh] ml-[5vw] mr-[5vw] rounded-xl bg-black">
           <div className="w-full p-10 text-white">
             <div className="flex">
-              <div className="w-[20vw] bg-gray-200 p-10">
+              <div className="w-[20vw] bg-gray-200 p-10 flex justify-center">
                 <div className="w-[10vw] h-[10vw] bg-red-100"></div>
               </div>
-              <div className="w-[80vw] bg-gray-400 p-10">
-                sodudsafdnaslfbslbdlshbhl
+              <div className="w-[80vw] bg-gray-400 p-10 flex flex-col">
+                <div>sodudsafdnaslfbslbdlshbhl</div>
+                <div>sodudsafdnaslfbslbdlshbhl</div>
+                <div>sodudsafdnaslfbslbdlshbhl</div>
+                <div>sodudsafdnaslfbslbdlshbhl</div>
+                <div>sodudsafdnaslfbslbdlshbhl</div>
+                <div>sodudsafdnaslfbslbdlshbhl</div>
+                <div>sodudsafdnaslfbslbdlshbhl</div>
+                <div>sodudsafdnaslfbslbdlshbhl</div>
+                <div>sodudsafdnaslfbslbdlshbhl</div>
+                <div>sodudsafdnaslfbslbdlshbhl</div>
+                <div>sodudsafdnaslfbslbdlshbhl</div>
+                <div>sodudsafdnaslfbslbdlshbhl</div>
+                <div>sodudsafdnaslfbslbdlshbhl</div>
+                <div>sodudsafdnaslfbslbdlshbhl</div>
+                <div>sodudsafdnaslfbslbdlshbhl</div>
+                <div>sodudsafdnaslfbslbdlshbhl</div>
               </div>
             </div>
           </div>
